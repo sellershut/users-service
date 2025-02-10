6d13209882aeafba59b177a99d1354eb5d55df1c
rtkay123 <dev@kanjala.com>
2025-02-10 22:29:05 +0200

    feat: query users

--- src/state/query.rs
+++ src/state/query.rs
@@ -8,6 +8,8 @@ use sellershut_core::{
 use tonic::{Request, Response, Status};
 use tracing::instrument;
 
+use crate::entity;
+
 use super::AppState;
 
 #[tonic::async_trait]
@@ -17,22 +19,62 @@ impl QueryUsers for AppState {
         &self,
         _request: Request<google::protobuf::Empty>,
     ) -> Result<Response<QueryUsersResponse>, Status> {
-        todo!()
+        let user = sqlx::query_as!(
+            entity::User,
+            "select * from \"user\" where local = $1",
+            true
+        )
+        .fetch_all(&self.services.postgres)
+        .await
+        .map_err(|e| tonic::Status::unavailable(e.to_string()))?;
+
+        let resp = QueryUsersResponse {
+            users: user.into_iter().map(Into::into).collect(),
+        };
+
+        Ok(tonic::Response::new(resp))
     }
 
     #[instrument(skip(self), err(Debug))]
     async fn query_user_by_name(
         &self,
-        _request: Request<QueryUserByNameRequest>,
+        request: Request<QueryUserByNameRequest>,
     ) -> Result<Response<QueryUserByNameResponse>, Status> {
-        todo!()
+        let username = request.into_inner().username;
+
+        let user = sqlx::query_as!(
+            entity::User,
+            "select * from \"user\" where username = $1 and local = $2",
+            username,
+            true
+        )
+        .fetch_optional(&self.services.postgres)
+        .await
+        .map_err(|e| tonic::Status::unavailable(e.to_string()))?;
+
+        let resp = QueryUserByNameResponse {
+            user: user.map(Into::into),
+        };
+
+        Ok(tonic::Response::new(resp))
     }
 
     #[instrument(skip(self), err(Debug))]
     async fn query_user_by_id(
         &self,
-        _request: Request<QueryUserByIdRequest>,
+        request: Request<QueryUserByIdRequest>,
     ) -> Result<Response<QueryUserByIdResponse>, Status> {
-        todo!()
+        let id = request.into_inner().id;
+
+        let user = sqlx::query_as!(entity::User, "select * from \"user\" where ap_id = $1", id)
+            .fetch_optional(&self.services.postgres)
+            .await
+            .map_err(|e| tonic::Status::unavailable(e.to_string()))?;
+
+        let resp = QueryUserByIdResponse {
+            user: user.map(Into::into),
+        };
+
+        Ok(tonic::Response::new(resp))
     }
 }
